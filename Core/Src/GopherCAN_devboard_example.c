1d457c1c495c5a418ab69ca896575c64fc0b7911
alext2052 <alextong2004@gmail.com>
2024-04-25 16:42:43 -0500

    Comment

--- Core/Src/GopherCAN_devboard_example.c
+++ Core/Src/GopherCAN_devboard_example.c
@@ -41,7 +41,7 @@ int setup1, setup2;
 //  What needs to happen on startup in order to run GopherCAN
 void init(CAN_HandleTypeDef* hcan_ptr)
 {
-	HAL_GPIO_WritePin(Pullup_1_GPIO_Port, Pullup_1_Pin, 0);
+//	HAL_GPIO_WritePin(Pullup_1_GPIO_Port, Pullup_1_Pin, 0);
 	example_hcan = hcan_ptr;
 
 	// initialize CAN
@@ -119,7 +119,7 @@ void main_loop()
 		printf("Current tick: %lu\n", HAL_GetTick());
 		last_print_hb = HAL_GetTick();
 		HAL_GPIO_TogglePin(HBeat_GPIO_Port, HBeat_Pin);
-//		HAL_GPIO_TogglePin(Pullup_1_GPIO_Port, Pullup_1_Pin);
+		HAL_GPIO_TogglePin(Pullup_1_GPIO_Port, Pullup_1_Pin);
 		HAL_GPIO_TogglePin(Pullup_2_GPIO_Port, Pullup_2_Pin);
 		HAL_GPIO_TogglePin(Pullup_3_GPIO_Port, Pullup_3_Pin);
 		HAL_GPIO_TogglePin(PU4_GPIO_Port, PU4_Pin);
@@ -132,10 +132,6 @@ void main_loop()
 		HAL_GPIO_TogglePin(PU11_GPIO_Port, PU11_Pin);
 		HAL_GPIO_TogglePin(PU12_GPIO_Port, PU12_Pin);
 		HAL_GPIO_TogglePin(GSense_GPIO_Port, GSense_Pin);
-
-
-
-
 	}
 
 //	if (check_pulse_sensors() != NO_PULSE_SENSOR_ISSUES) {
