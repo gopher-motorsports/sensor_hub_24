991093ccdd6c8b214de12d31a41e0254f431c14b
alext2052 <alextong2004@gmail.com>
2024-09-21 22:48:50 -0500

    UpdateCubeMX

--- Core/Src/GopherCAN_devboard_example.c
+++ Core/Src/GopherCAN_devboard_example.c
@@ -79,11 +79,11 @@ void init(CAN_HandleTypeDef* hcan_ptr)
 	HAL_GPIO_WritePin(PU2_GPIO_Port, PU2_Pin, 1); // NC
 	HAL_GPIO_WritePin(PU3_GPIO_Port, PU3_Pin, 1); // NC
 	HAL_GPIO_WritePin(PU4_GPIO_Port, PU4_Pin, 1); // NC
-	HAL_GPIO_WritePin(PU5_GPIO_Port, PU5_Pin, 1); // Air Line Pressure - voltage
-	HAL_GPIO_WritePin(PU6_GPIO_Port, PU6_Pin, 1); // Air Tank Pressure - voltage
-	HAL_GPIO_WritePin(PU7_GPIO_Port, PU7_Pin, 0); // Manifold Temp - temperature
-	HAL_GPIO_WritePin(PU8_GPIO_Port, PU8_Pin, 1); // Manifold Pressure - voltage
-	HAL_GPIO_WritePin(PU9_GPIO_Port, PU9_Pin, 1); // Shock Pos Rear Right - voltage
+	HAL_GPIO_WritePin(PU5_GPIO_Port, PU5_Pin, 1); //
+	HAL_GPIO_WritePin(PU6_GPIO_Port, PU6_Pin, 1); //
+	HAL_GPIO_WritePin(PU7_GPIO_Port, PU7_Pin, 1); //
+	HAL_GPIO_WritePin(PU8_GPIO_Port, PU8_Pin, 1); //
+	HAL_GPIO_WritePin(PU9_GPIO_Port, PU9_Pin, 1); //
 	HAL_GPIO_WritePin(PU10_GPIO_Port, PU10_Pin, 1); // NC
 	HAL_GPIO_WritePin(PU11_GPIO_Port, PU11_Pin, 1); // NC
 	HAL_GPIO_WritePin(PU12_GPIO_Port, PU12_Pin, 1); // NC
